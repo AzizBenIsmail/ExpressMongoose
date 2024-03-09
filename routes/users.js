a8aaf76f4718373a163565051711e3bfdac85374
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-09 13:20:03 +0100

    partie crud séance 2 GetAll / AddUser

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('../models/userSchema');
 
-/* GET users listing. */
-router.get('/getAll', function(req, res, next) {
-  res.json('recuperation des donnee'); 
+router.get('/', async function(req, res, next) {
+  try {
+       const userList = await userModel.find();
+      if(!userList)
+      {
+        throw new Error('User not found');
+      }
+      console.log(userList);
+      res.status(200).json(userList);
+  }catch(err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
-/* GET users listing. */
-router.get('/getAll/:id', function(req, res, next) {
-  res.json('recuperation des donnee'); 
-});
-
-/* POST users listing. */
-router.post('/addFormation', function(req, res, next) {
-  res.json('add');
-});
-
-/* PUT users listing. */
-router.put('/updateFormation/:id', function(req, res, next) {
-  res.json('update');
-});
-
-/* DELETE users listing. */
-router.delete('/deleteFormation', function(req, res, next) {
-  res.json('delete');
+router.post('/', async function(req, res, next) {
+  try {    
+    console.log(req.body);
+    const {nom , prenom , age , address} = req.body;
+    const user= new userModel({
+      nom , prenom , age , address
+    })
+    const usersadded = await user.save();
+      res.status(200).json(usersadded);
+  }catch(err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
 module.exports = router;
