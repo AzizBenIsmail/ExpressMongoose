472b625f9ebd5cba33def03bff4f8c4f55d48a59
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-09 09:57:23 +0100

    type de routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getAll', function(req, res, next) {
+  res.json('recuperation des donnee'); 
+});
+
+/* GET users listing. */
+router.get('/getAll/:id', function(req, res, next) {
+  res.json('recuperation des donnee'); 
+});
+
+/* POST users listing. */
+router.post('/addFormation', function(req, res, next) {
+  res.json('add');
+});
+
+/* PUT users listing. */
+router.put('/updateFormation/:id', function(req, res, next) {
+  res.json('update');
+});
+
+/* DELETE users listing. */
+router.delete('/deleteFormation', function(req, res, next) {
+  res.json('delete');
 });
 
 module.exports = router;
