abfe76cc157d11eedd4c5111586f9b96021d5b06
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-03-09 13:19:42 +0100

    test des routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.json('hello world');
 });
 
+/* GET users listing. */
+router.get('/getAll', function(req, res, next) {
+  res.json('recuperation des donnee'); 
+});
+
+/* GET users listing. */
+router.get('/getAll/:id', function(req, res, next) {
+  res.json('recuperation des donnee'); 
+});
+
+/* POST users listing. */
+router.post('/addFormation', function(req, res, next) {
+  res.json('add');
+});
+
+/* PUT users listing. */
+router.put('/updateFormation/:id', function(req, res, next) {
+  res.json('update');
+});
+
+/* DELETE users listing. */
+router.delete('/deleteFormation', function(req, res, next) {
+  res.json('delete');
+});
+
 module.exports = router;
